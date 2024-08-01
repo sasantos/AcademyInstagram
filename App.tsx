fabc32b19108d2c2097ece0e31f27b65fc5a059b
Sidney A Dos Santos <sidney.sasantos@outlook.com>
2024-08-01 10:03:35 -0300

    create theme with colors e fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import colors from "./src/theme/colors";
+import font from "./src/theme/fonts";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hello Universe!!</Text>
+      <AntDesign name="home" size={50} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.primary,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: font.size.xlg,
   },
 });
